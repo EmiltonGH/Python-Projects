9c5f5c419d3cdf7029ed7ee73c7202b8a6a885e0
Emilton Soosaippillai <emiltons@icloud.com>
2024-10-08 07:10:23 +0100

    Updates scripts with additional enhancements

--- rolling_dice.py
+++ rolling_dice.py
@@ -1,13 +1,15 @@
 import random
 
+roll_count = 0
 while True:
-    choice = input("Roll the dice? (y/n): ").lower()
+    choice = input("Are you ready to roll the dice? (y/n): ").lower()
     if choice == 'y':
-        die1 = random.randint(1,6)
-        die2 = random.randint(1,6)
-        print(f'({die1}, {die2})')
+        number_of_dice = int(input('How many dice would you like to roll?: '))
+        dice_rolls = [random.randint(1,6) for _ in range(number_of_dice)]
+        print(f'You rolled {dice_rolls}')
+        roll_count += 1
     elif choice == 'n':
-        print('Thanks for playing!')
+        print(f'Thanks for playing! You rolled the dice {roll_count} times.')
         break
     else:
         print('Invalid choice!')
